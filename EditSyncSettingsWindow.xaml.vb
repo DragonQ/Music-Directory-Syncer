b865fe5aaa713df43c4f7ce5786c97aaa5c0f5b6
Adam Godbeer <Adam Godbeer>
2016-02-05 18:50:22 +0000

    Edit Sync Window: Settings are now saved when user clicks on "Save".

--- EditSyncSettingsWindow.xaml.vb
+++ EditSyncSettingsWindow.xaml.vb
@@ -1,5 +1,6 @@
 ﻿#Region " Namespaces "
 Imports Music_Folder_Syncer.Logger.DebugLogLevel
+Imports Microsoft.WindowsAPICodePack.Dialogs
 #End Region
 
 Public Class EditSyncSettingsWindow
@@ -15,22 +16,88 @@ Public Class EditSyncSettingsWindow
         spinThreads.Value = MySyncSettings.MaxThreads
 
     End Sub
+    Private Function CreateDirectoryBrowser() As ReturnObject
+
+        Dim SelectDirectoryDialog = New CommonOpenFileDialog()
+        SelectDirectoryDialog.Title = "Select Sync Directory"
+        SelectDirectoryDialog.IsFolderPicker = True
+        SelectDirectoryDialog.AddToMostRecentlyUsedList = False
+        SelectDirectoryDialog.AllowNonFileSystemItems = True
+        SelectDirectoryDialog.DefaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        SelectDirectoryDialog.EnsureFileExists = False
+        SelectDirectoryDialog.EnsurePathExists = True
+        SelectDirectoryDialog.EnsureReadOnly = False
+        SelectDirectoryDialog.EnsureValidNames = True
+        SelectDirectoryDialog.Multiselect = False
+        SelectDirectoryDialog.ShowPlacesList = True
+
+        If SelectDirectoryDialog.ShowDialog() = CommonFileDialogResult.Ok Then
+            Return New ReturnObject(True, "", SelectDirectoryDialog.FileName)
+        Else
+            Return New ReturnObject(False, "")
+        End If
+
+    End Function
 
 #Region " Window Controls "
+    Private Sub btnBrowseSourceDirectory_Click(sender As Object, e As RoutedEventArgs) Handles btnBrowseSourceDirectory.Click
+
+        Dim Browser As ReturnObject = CreateDirectoryBrowser()
+
+        If Browser.Success Then
+            txtSourceDirectory.Text = CStr(Browser.MyObject)
+        End If
+
+    End Sub
+
+    Private Sub btnBrowseSyncDirectory_Click(sender As Object, e As RoutedEventArgs) Handles btnBrowseSyncDirectory.Click
+
+        Dim Browser As ReturnObject = CreateDirectoryBrowser()
+
+        If Browser.Success Then
+            txtSyncDirectory.Text = CStr(Browser.MyObject)
+        End If
+
+    End Sub
+
     Private Sub btnSave_Click(sender As Object, e As RoutedEventArgs)
 
         EnableDisableControls(False)
-        Dim MyResult As ReturnObject = SaveSyncSettings()
 
-        If MyResult.Success Then
-            MyLog.Write("Syncer settings updated.", Information)
-            Me.DialogResult = True
-            Me.Close()
-        Else
-            MyLog.Write("Could not update sync settings. Error: " & MyResult.ErrorMessage, Warning)
-            System.Windows.MessageBox.Show("Save failed!", MyResult.ErrorMessage, MessageBoxButton.OK, MessageBoxImage.Error)
+        Try
+            ' Check settings aren't nonsense
+            If Not IO.Directory.Exists(txtSourceDirectory.Text) Then
+                Throw New Exception("Specified source directory doesn't exist.")
+            End If
+
+            If Not IO.Directory.Exists(txtSyncDirectory.Text) Then
+                If (IO.Path.IsPathRooted(txtSyncDirectory.Text) AndAlso IO.Path.IsPathRooted(txtSyncDirectory.Text)) Then
+                    IO.Directory.CreateDirectory(txtSyncDirectory.Text)
+                Else
+                    Throw New Exception("Specified sync directory is not valid.")
+                End If
+            End If
+
+            ' Apply settings
+            MySyncSettings.SourceDirectory = txtSourceDirectory.Text
+            MySyncSettings.SyncDirectory = txtSyncDirectory.Text
+            MySyncSettings.MaxThreads = CInt(spinThreads.Value)
+
+            ' Save settings to file
+            Dim MyResult As ReturnObject = SaveSyncSettings()
+
+            If MyResult.Success Then
+                MyLog.Write("Syncer settings updated.", Information)
+                Me.DialogResult = True
+                Me.Close()
+            Else
+                Throw New Exception(MyResult.ErrorMessage)
+            End If
+        Catch ex As Exception
+            MyLog.Write("Could not update sync settings. Error: " & ex.Message, Warning)
+            System.Windows.MessageBox.Show("Save failed!", ex.Message, MessageBoxButton.OK, MessageBoxImage.Error)
             EnableDisableControls(True)
-        End If
+        End Try
 
     End Sub
 
